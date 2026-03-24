a9af8ad8bd4d8ca0994bcc3eb9eaf4a0c9f07c3d
sagarthokala <thokalasagar2@gmail.com>
2026-03-25 00:00:33 +0530

    grpc client implementation for patient service

--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -4,9 +4,12 @@ import com.pm.patient_service.dto.PatientRequestDto;
 import com.pm.patient_service.dto.PatientResponseDto;
 import com.pm.patient_service.exception.EmailAlreadyExitsException;
 import com.pm.patient_service.exception.PatientNotFoundException;
+import com.pm.patient_service.grpc.BillingServiceGrpcClient;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -16,10 +19,13 @@ import java.util.UUID;
 @Service
 public class PatientService {
 
-    private PatientRepository patientRepository;
+    private static final Logger log = LoggerFactory.getLogger(PatientService.class);
+    private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
 
-    public PatientService(PatientRepository patientRepository) {
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
     }
 
     //fetch all the patients from db
@@ -37,6 +43,11 @@ public class PatientService {
             throw new EmailAlreadyExitsException("A patient with this EmailId already exists");
         }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDto));
+
+        log.info("entering grpc method ");
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),
+                newPatient.getName(), newPatient.getEmail());
+        log.info("exiting grpc method ");
 
         return PatientMapper.toDTO(newPatient);
     }
