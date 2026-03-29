6f2cac57e4ad624c3114496ab4ac648dc97afcaa
sagarthokala <thokalasagar2@gmail.com>
2026-03-29 20:40:16 +0530

    addign auth service setup intial code

---
+++ auth-service/src/main/java/com/pm/auth_service/dto/LoginRequestDTO.java
@@ -0,0 +1,18 @@
+package com.pm.auth_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "password must be atleast 8 char length")
+    private String password;
+}
